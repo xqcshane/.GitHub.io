6d08d1c42042984a927992fb37fbf5f63ee1b2e6
xqcshane <83743200+xqcshane@users.noreply.github.com>
2021-05-19 06:47:25 +0800

    Delete web-mobile directory

--- web-mobile/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ballInGame","ballInRecycle"],collisionMatrix:[[true,true,true],[true,false],[true,false,true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"portrait",jsList:[]};
