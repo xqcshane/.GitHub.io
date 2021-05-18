0aff349a659af2dca40d374270cef33376ec0ada
xqcshane <83743200+xqcshane@users.noreply.github.com>
2021-05-19 06:46:44 +0800

    Add files via upload

---
+++ pinball/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ballInGame","ballInRecycle"],collisionMatrix:[[true,true,true],[true,false],[true,false,true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/home.fire",orientation:"portrait",jsList:[]};
