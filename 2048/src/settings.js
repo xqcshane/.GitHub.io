0c3977e85e5db9db8ef535ab16aee7aed9d41ce8
xqcshane <83743200+xqcshane@users.noreply.github.com>
2021-05-19 15:34:43 +0800

    Add files via upload

--- 2048/src/settings.js
+++ 2048/src/settings.js
@@ -1,1 +1,1 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[]};
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/main.fire",orientation:"portrait",jsList:[]};
